be64b5d962a685e894df63984cb25dceae1f2555
mantiss <maniidon7890@gmail.com>
2025-07-29 00:00:51 +0530

    delete_useless_words

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ function App() {
   return (
     <>
       <div>
-        fourth Commit of hash make this dev branch KArik commit another branch
-        helo kartik
-        created
+        
       </div>
     </>
   );
