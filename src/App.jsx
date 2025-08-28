ea8f951a7d266a53badde283f85a1e3c8e6a1428
mantiss <maniidon7890@gmail.com>
2025-08-28 17:26:04 +0530

    kartik start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route,Routes } from "react-router-dom";
+import Linking from "./Pages/linking";
+import Home from "./Pages/home";
+import About from "./Pages/about";
+
 function App() {
   return (
     <>
       <div>
+      <Linking/>
+        <Routes>
+        <Route path="/home-page" element={<Home/>}></Route>
+        <Route path="/about-page" element={<About/>}></Route>
+        </Routes>
+        
       </div>
     </>
   );
---
+++ src/Pages/about.jsx
@@ -0,0 +1,13 @@
+function About(){
+    return(
+        <>
+        <div>
+            <p className=" text-center items-center justify-center text-5xl flex flex-col">Hello Its About Us Page</p>
+            <p className=" text-center items-center justify-center text-5xl flex flex-col">When ever you push the code before this every time give a pull all code otherwise we got issues </p>
+
+        </div>
+        </>
+    )
+}
+
+export default About
