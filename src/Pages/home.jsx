856e9c19d2e1195abfa7f27529ae2b0a6eb40c0c
BxFT <BxFT@bft.com>
2025-08-31 00:16:34 +0530

    ready to work

---
+++ src/Pages/home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <>
+        <div>
+            <p className="text-6xl flex flex-col justify-center text-center items-center">Hello its HomePage</p>
+        </div>
+        </>
+    )
+}
+
+export default Home
